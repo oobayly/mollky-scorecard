282c41f3c0337d5ca6a5539c95a142cc358ad941
John Bayly <3430117+oobayly@users.noreply.github.com>
2020-07-07 17:24:27 +0100

    Add loading spinners

--- webapp/src/app/shared/shared.module.ts
+++ webapp/src/app/shared/shared.module.ts
@@ -2,13 +2,14 @@ import { BadgeModule, BreadcrumbModule, ButtonsModule, IconsModule, InputsModule
 
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { LoaderComponent } from "./loader/loader.component";
 import { NgModule } from "@angular/core";
 import { PlayersListComponent } from "./players-list/players-list.component";
 import { QRCodeModule } from "angularx-qrcode";
 import { ZXingScannerModule } from "@zxing/ngx-scanner";
 
 @NgModule({
-  declarations: [PlayersListComponent],
+  declarations: [LoaderComponent, PlayersListComponent],
   imports: [
     CommonModule,
     FormsModule,
@@ -31,6 +32,7 @@ import { ZXingScannerModule } from "@zxing/ngx-scanner";
     IconsModule,
     InputsModule,
     // Components
+    LoaderComponent,
     PlayersListComponent,
     // Other
     QRCodeModule,
