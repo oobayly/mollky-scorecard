e6b6e8a060ce5e7e059931a9287b205f0a7f28ac
John Bayly <3430117+oobayly@users.noreply.github.com>
2021-06-14 12:14:55 +0100

    Use emulators for local development

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,22 +1,24 @@
+import { registerLocaleData } from "@angular/common";
+import localeGB from "@angular/common/locales/en-GB"
 import { LOCALE_ID, NgModule } from "@angular/core";
-
 import { AngularFireModule } from "@angular/fire";
-import { AppComponent } from "./app.component";
-import { AppRoutingModule } from "./app-routing.module";
+import { USE_EMULATOR as USE_AUTH_EMULATOR } from "@angular/fire/auth";
+import { USE_EMULATOR as USE_FIRESTORE_EMULATOR } from "@angular/fire/firestore";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-import { HomeComponent } from "./pages/home/home.component";
+import { BrowserModule } from "@angular/platform-browser";
 import {
   MDBBootstrapModule,
 } from "angular-bootstrap-md";
+import { ServiceWorkerModule } from "@angular/service-worker";
+
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { HomeComponent } from "./pages/home/home.component";
 import { ModalsModule } from "./modals/modals.module";
 import { PlayersComponent } from "./pages/players/players.component";
-import { ServiceWorkerModule } from "@angular/service-worker";
 import { SharedModule } from "./shared/shared.module";
 import { environment } from "../environments/environment";
-import localeGB from "@angular/common/locales/en-GB"
-import { registerLocaleData } from "@angular/common";
 
 registerLocaleData(localeGB);
 
@@ -47,6 +49,8 @@ registerLocaleData(localeGB);
   ],
   providers: [
     { provide: LOCALE_ID, useValue: navigator.language },
+    { provide: USE_AUTH_EMULATOR, useValue: environment.production ? undefined : ["localhost", 9099] },
+    { provide: USE_FIRESTORE_EMULATOR, useValue: environment.production ? undefined : ["localhost", 8080] },
   ],
   bootstrap: [AppComponent],
 })
